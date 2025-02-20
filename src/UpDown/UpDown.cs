e0f33330c17cef7bbd211d692a1d4b5c26a0e11f
naylorw <83007104+naylorw@users.noreply.github.com>
2025-02-20 14:51:08 +0000

    Initial commit
    Initial setup of the adaptive psychophysics library.
    Preliminary functionality includes the Up/Down method as well as the
    Weighted Up/Down and Accelerated Stochastic Approximation (ASA)

---
+++ src/UpDown/UpDown.cs
@@ -0,0 +1,46 @@
+﻿namespace AdaptivePsychophysicsLibrary.UpDown
+{
+	public class UpDown : AdaptiveProcedure
+	{
+		public readonly double stepSize;
+
+		public UpDown(double initialValue, double stepSize, EndCondition endCondition, int endConditionValue)
+		{
+			testedValues.Add(initialValue);
+			this.stepSize = stepSize;
+			this.endCondition = endCondition;
+			this.endConditionValue = endConditionValue;
+		}
+
+		public override double NextValue()
+		{
+			if (testedValues.Count > responses.Count)
+			{
+				return testedValues[^1];
+			}
+
+			double Zn;
+			switch (responses[^1])
+			{
+				case ResponseState.CorrectResp:
+					Zn = 1;
+					break;
+				case ResponseState.WrongResp:
+					Zn = 0;
+					break;
+				case ResponseState.DontKnowResp:
+					Zn = 0.5;
+					break;
+				default:
+					throw new ArgumentException();
+			}
+
+			double stepModifier = 2 * Zn - 1;
+			double nextValue = testedValues[^1] - stepSize * stepModifier;
+
+			testedValues.Add(nextValue);
+
+			return nextValue;
+		}
+	}
+}
---
+++ src/WeightedUpDown/WeightedUpDown.cs
@@ -0,0 +1,57 @@
+﻿namespace AdaptivePsychophysicsLibrary.WeightedUpDown
+{
+	internal class WeightedUpDown : AdaptiveProcedure
+	{
+		public readonly double targetProbability;
+		public readonly double stepSize;
+		public WeightedUpDown(double initialValue, double stepSize, double targetProbability, EndCondition endCondition, int endConditionValue)
+		{
+			testedValues.Add(initialValue);
+			this.stepSize = stepSize;
+			this.targetProbability = targetProbability;
+			this.endCondition = endCondition;
+			this.endConditionValue = endConditionValue;
+		}
+
+		public override double NextValue()
+		{
+			if (testedValues.Count > responses.Count)
+			{
+				return testedValues[^1];
+			}
+
+			double Zn;
+			switch (responses[^1])
+			{
+				case ResponseState.CorrectResp:
+					Zn = 1;
+					break;
+				case ResponseState.WrongResp:
+					Zn = 0;
+					break;
+				case ResponseState.DontKnowResp:
+					Zn = 0.5;
+					break;
+				default:
+					throw new ArgumentException();
+			}
+
+			double stepModifier = Zn - targetProbability;
+			// Rescale the step sizes so the smaller step size is equal to the provided step size
+			if (Math.Abs(1-targetProbability) < Math.Abs(0 - targetProbability))
+			{
+				stepModifier /= Math.Abs(1 - targetProbability);
+			}
+			else
+			{
+				stepModifier /= Math.Abs(0 - targetProbability);
+			}
+
+			double nextValue = testedValues[^1] - stepSize * stepModifier;
+
+			testedValues.Add(nextValue);
+
+			return nextValue;
+		}
+	}
+}
