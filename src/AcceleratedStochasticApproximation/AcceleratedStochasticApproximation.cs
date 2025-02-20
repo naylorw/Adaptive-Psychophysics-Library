2acf037834476f677bf29312a9635943040dce59
naylorw <williamnaylor123@googlemail.com>
2025-02-20 14:23:56 +0000

    Initial setup
    Initial setup of the adaptive psychophysics library.
    Preliminary functionality includes the Up/Down method as well as the
    Weighted Up/Down and Accelerated Stochastic Approximation (ASA)

---
+++ src/AcceleratedStochasticApproximation/AcceleratedStochasticApproximation.cs
@@ -0,0 +1,53 @@
+﻿namespace AdaptivePsychophysicsLibrary.AcceleratedStochasticApproximation
+{
+	internal class AcceleratedStochasticApproximation : AdaptiveProcedure
+	{
+		public readonly double c;
+		public readonly double targetProbability;
+		public AcceleratedStochasticApproximation(double initialValue, double c, double targetProbability, EndCondition endCondition, int endConditionValue)
+		{
+			testedValues.Add(initialValue);
+			this.c = c;
+			this.targetProbability = targetProbability;
+			this.endCondition = endCondition;
+			this.endConditionValue = endConditionValue;
+		}
+
+		public override double NextValue()
+		{
+			double _Zn;
+			switch (responses[^1])
+			{
+				case ResponseState.WrongResp:
+					_Zn = 0;
+					break;
+				case ResponseState.CorrectResp:
+					_Zn = 1;
+					break;
+				case ResponseState.DontKnowResp:
+					_Zn = 0.5;
+					break;
+				default:
+					_Zn = -100;
+					Console.WriteLine($"Unknown ResponseState: {responses[^1]}");
+					break;
+			}
+
+			double _shiftFactor;
+			if (responses.Count > 2)
+			{
+				_shiftFactor = c / (2 + GetInversionCount());
+			}
+			else
+			{
+				_shiftFactor = c / responses.Count;
+			}
+
+			double nextValue = testedValues[^1] - _shiftFactor * (_Zn - targetProbability);
+
+			testedValues.Add(nextValue);
+
+			return nextValue;
+		}
+	}
+}
---
+++ src/AdaptiveProcedureBase.cs
@@ -0,0 +1,128 @@
+﻿namespace AdaptivePsychophysicsLibrary
+{
+	public abstract class AdaptiveProcedure
+	{
+		#region Variables
+		public List<double> testedValues = new();
+		public List<ResponseState> responses = new();
+		public bool AllowDontKnowResponses { get { return _allowDontKnowResponses; } set { _allowDontKnowResponses = value; } }
+		private bool _allowDontKnowResponses = true;
+		public enum ResponseState
+		{
+			WrongResp = 0,
+			CorrectResp = 1,
+			DontKnowResp = 2
+		}
+
+		public enum EndCondition
+		{
+			NumberOfTrials,
+			NumberOfInversions
+		}
+		protected EndCondition endCondition;
+		protected int endConditionValue;
+		#endregion
+
+		#region Functions
+		public void AddResult(ResponseState responseState)
+		{
+			if (testedValues.Count != responses.Count + 1)
+			{
+				Console.WriteLine("Error adding result when a new result was not expected");
+				return;
+			}
+
+			if (responseState == ResponseState.DontKnowResp && !AllowDontKnowResponses)
+			{
+				Console.WriteLine("Response Don't Know provided when these have been prohibited by the AllowDontKnowResponses variable");
+				return;
+			}
+
+			responses.Add(responseState);
+		}
+
+		public void SetTestedValuesHistory(List<double> testedValues, List<ResponseState> responses)
+		{
+			if (testedValues.Count != responses.Count)
+			{
+				Console.WriteLine("Error setting pre-solved tested values, testedValues count does not equal responses count. This function should only be used during initialisation of an adaptive procedure.");
+				return;
+			}
+			this.testedValues = testedValues;
+			this.responses = responses;
+		}
+
+		public abstract double NextValue();
+
+		public bool IsFinished()
+		{
+			return endCondition switch
+			{
+				EndCondition.NumberOfTrials => CheckNumberOfTrialsEndCondition(),
+				EndCondition.NumberOfInversions => CheckNumberOfInversionsEndCondition(),
+				_ => throw new ArgumentException("Unhandled EndCondition: " + endCondition.ToString()),
+			};
+		}
+		private bool CheckNumberOfTrialsEndCondition()
+		{
+			if (responses.Count >= endConditionValue)
+				return true;
+			else return false;
+		}
+		private bool CheckNumberOfInversionsEndCondition()
+		{
+			if (GetInversionCount() >= endConditionValue) return true;
+			else return false;
+		}
+
+		protected int GetInversionCount()
+		{
+			int inversions = 0;
+			bool currentDirection = responses[0] == ResponseState.CorrectResp;  // false is up, true is down
+			for (int i = 0; i < responses.Count; i++)
+			{
+				switch (responses[i])
+				{
+					case ResponseState.WrongResp:
+					case ResponseState.DontKnowResp:
+						if (currentDirection)
+						{
+							inversions++;
+							currentDirection = false;
+						}
+						break;
+					case ResponseState.CorrectResp:
+						if (!currentDirection)
+						{
+							inversions++;
+							currentDirection = true;
+						}
+						break;
+					default:
+						throw new ArgumentOutOfRangeException("Unexpected response state: " + responses[i].ToString());
+				}
+			}
+
+			return inversions;
+		}
+		#endregion
+
+		#region Result Analysis
+		public double GetAverageOfLastTrials(int numberOfTrials)
+		{
+			if (numberOfTrials >= testedValues.Count)
+			{
+				return testedValues.Average();
+			}
+
+			double sum = 0;
+			for (int i = 1; i <= numberOfTrials; i++)
+			{
+				sum += testedValues[^i];
+			}
+
+			return sum / numberOfTrials;
+		}
+		#endregion
+	}
+}
